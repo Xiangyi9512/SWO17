70a79b1c414a7cb19ebc2a77576a3a8def073ddd
TobyUCL <litaolun@hotmail.com>
2015-11-04 12:28:27 +0000

    Added todo.html

--- ToDoList/WebApp/views.py
+++ ToDoList/WebApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 
@@ -10,5 +10,6 @@ def listoftodo(request):
 	return render(request, 'WebApp/index.html', context)
 
 def todo(request, todolist_id):
-	return HttpResponse("You're looking at todo list %s." % todolist_id)
+	question = get_object_or_404(ToDoList, pk=todolist_id)
+	return render(request, "WebApp/todo.html", {'todolist': todolist})
 
