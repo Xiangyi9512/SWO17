f0c1c3dfd44d966a01212bea1508aee3b3486bdc
TobyUCL <litaolun@hotmail.com>
2015-11-05 14:36:20 +0000

    now returns to main page after todo list is saved

--- ToDoList/WebApp/forms.py
+++ ToDoList/WebApp/forms.py
@@ -5,12 +5,6 @@ class ToDoForm(forms.ModelForm):
 	class Meta:
 		model = ToDoList
 		fields = ['title', 'content']
-
-	# def __init__(self, *args, **kwargs):
-	# 	todoinfo = kwargs.pop('todoinfo')
-	# 	super(ToDoForm, self).__init__(*args, **kwargs)
-	# 	self.fields['title'].initial = todoinfo
-
 	title = forms.CharField(required=True, max_length=100)
 	content = forms.CharField(required=False, widget=forms.Textarea)
 
--- ToDoList/WebApp/views.py
+++ ToDoList/WebApp/views.py
@@ -53,6 +53,7 @@ def newtodo(request, users_id):
 		instance.date = timezone.now()
 		instance.user_id = user.id
 		instance.save()
+		return HttpResponseRedirect("/WebApp/%s" % user.id)
 	context = {
 		'user': user,
 		'form': form,
@@ -72,6 +73,7 @@ def todo(request, users_id, todolist_id):
 		todo.title = instance.title
 		todo.content = instance.content
 		todo.save()
+		return HttpResponseRedirect("/WebApp/%s" % user.id)
 	context = {
 		'todo': todo,
 		'user': user,
