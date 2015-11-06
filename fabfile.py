428b3326ceaaa83c1158a0adfb7a9e8e30a2e18f
XinyueZhang1996 <uczlxz9@ucl.ac.uk>
2015-11-06 03:00:24 +0000

    test fab function

--- fabfile.py
+++ fabfile.py
@@ -16,7 +16,7 @@ def deploy():
 
 
 def connected():
-	code_dir = '/desktop/test1/var'
+	code_dir = '/Desktop/test1/var'
     	with cd(code_dir):
         	run("git clone https://github.com/Xiangyi9512/SWO17.git ")
         	run("touch app.wsgi")
