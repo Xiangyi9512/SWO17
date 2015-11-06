a96a43b493249ae887318708abc7d414b9f4347c
XinyueZhang1996 <uczlxz9@ucl.ac.uk>
2015-11-06 02:12:01 +0000

    test fab function

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import env, local, run
 
 def pre_deploy():
     local("ls -a")
-    local("git add -p && git commit")
+    local("git add -A")
+    local("git commit -m 'test fab function'")
     local("git push")
 
