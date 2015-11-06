bfc7a8dfea71bd514a0533a74bd9c06ef08d4398
XinyueZhang1996 <uczlxz9@ucl.ac.uk>
2015-11-06 15:50:55 +0000

    test fab function

--- fabfile.py
+++ fabfile.py
@@ -27,14 +27,14 @@ def following_deploy():
     connected2()
 
 def connected():
-	code_dir = '/Desktop/'
+	code_dir = '~/Desktop/'
 
     	with cd(code_dir):
             run("git clone https://github.com/Xiangyi9512/SWO17.git")
         run("touch app.wsgi")
 
 def connected2():
-    code_dir = '/Desktop/SWO17'
+    code_dir = '~/Desktop/SWO17'
 
     with cd(code_dir):
             with settings(warn_only=True):
