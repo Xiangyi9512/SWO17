9190731d70702451ef463f953b60e4f5e6f59c0c
XinyueZhang1996 <uczlxz9@ucl.ac.uk>
2015-11-06 03:54:15 +0000

    test fab function

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
+from __future__ import with_statement
 from fabric.contrib.files import append, exists, sed
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import *
 from fabric.contrib.console import confirm
 
 #the one worked with the xinzhang VM
+env.hosts = ['xinzhang@newgate.cs.ucl.ac.uk']
 
 
 def pre_deploy():
@@ -24,5 +26,5 @@ def connected():
                 if run("test -d %s" % code_dir).failed:
                     run("git clone https://github.com/Xiangyi9512/SWO17.git")
         with cd(code_dir):
-            run("git pull")
-            run("touch app.wsgi")
+            run("git pull https://github.com/Xiangyi9512/SWO17.git")
+        run("touch app.wsgi")
